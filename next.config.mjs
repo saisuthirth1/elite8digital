cd02c358f0ec777e3ef64609cc18346d514de38b
saisuthirth1 <saisuthirth1347@gmail.com>
2025-05-24 18:40:12 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Elite8-Digital-' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/elite8digital' : '',
   images: {
     unoptimized: true,
   },
 }
-
-export default nextConfig
 
 export default nextConfig
